3c28d6d86b3dd12bedbbab6833fd8e066faba806
ThanhNhanNguyen <thanhnhan.b12002@gmail.com>
2023-03-22 22:11:00 +1100

    fix alignment and add dummy report data

--- src/pages/Reports/ReportList.js
+++ src/pages/Reports/ReportList.js
@@ -8,16 +8,32 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom'
 import Form from 'react-bootstrap/Form';
 
-const customers = [{id: 1, companyName: 'Company 1', ABN: '0123456789'},
-                    {id: 2, companyName: 'Company 2', ABN: '0123456789'},
+const com1Reports = [
+  {reportID: 1, reportType:'BAS', periodFrom:'11-02-2023', periodTo:'15-02-2023'},
+  {reportID: 2, reportType:'IAC', periodFrom:'11-02-2023', periodTo:'15-02-2023'},
+  {reportID: 3, reportType:'ITA', periodFrom:'11-02-2023', periodTo:'15-02-2023'},
+  {reportID: 4, reportType:'General Interest', periodFrom:'11-02-2023', periodTo:'15-02-2023'},
+];
+
+const com2Reports = [
+  {reportID: 5, reportType:'BAS', periodFrom:'11-02-2023', periodTo:'15-02-2023'},
+  {reportID: 6, reportType:'IAC', periodFrom:'11-02-2023', periodTo:'15-02-2023'},
+  {reportID: 7, reportType:'ITA', periodFrom:'11-02-2023', periodTo:'15-02-2023'},
+];
+
+const com4Reports = [
+  {reportID: 8, reportType:'General Interest', periodFrom:'11-02-2023', periodTo:'15-02-2023'},
+];
+
+const customers = [{id: 1, companyName: 'Company 1', ABN: '0123456789', report: com1Reports},
+                    {id: 2, companyName: 'Company 2', ABN: '0123456789', report: com2Reports},
                     {id: 3, companyName: 'Company 3', ABN: '0123456789'},
-                    {id: 4, companyName: 'Company 4', ABN: '0123456789'},
+                    {id: 4, companyName: 'Company 4', ABN: '0123456789', report: com4Reports},
                     {id: 5, companyName: 'Company 5', ABN: '0123456789'},
                     {id: 6, companyName: 'Company 6', ABN: '0123456789'},
                     ]
 
-const reportTypes = ['BAS', 'IAS', 'ICA', 'IAT'];
-
+const reportTypes = ['BAS', 'IAC', 'ITA', 'General Interest'];
 
 export default function ReportList() {
   let { requestId } = useParams();
@@ -51,16 +67,15 @@ export default function ReportList() {
           <div className="container">
             <div className="row border-bottom" style={{ marginBottom:'20px', padding:'0 0 20px 0', fontSize:'18px'}}>  
               <div className="col-1">
-
               </div>
               <div className="col-1" style={{ color:"#ee7170" }}>
-                ID
+                ABN
               </div>
+
               <div className="col-9" style={{ color:"#ee7170"  }}>
                 Customer Name
               </div>
               <div className="col-1">
-                
               </div>
             </div>
 
@@ -78,66 +93,65 @@ export default function ReportList() {
                     <div className="col-9">
                       {customer.companyName}
                     </div>
-                    <div className="col-1">
-                      <Form.Check inline type='checkbox' />
+                    <div className="col-1" style={{textAlign:'center'}}>
+                      <Form.Check inline type='checkbox' style={{margin:'0 0 0 0 '}}/>
                     </div>
               
-                      <Collapse in={open.includes(customer.id)}>
+                      <Collapse in={open.includes(customer.id)} style={{padding:'0px'}}>
                         <div>
-                          <div style={{padding:'0px 0 0 184px', margin:'30px 0 30px 0'}}>
+                          <div style={{padding:'0px 0 0 0px', margin:'30px 0 30px 0'}}>
                             <div className="container">
                               <div className="row" style={{fontSize:'15px'}}>
-                                <div className="col-2" >
+                                <div className="col-2">
+                                </div>
+                                <div className="col-1">
                                   <p >Report ID</p>
                                 </div>
-                                <div className="col-2">
+                                <div className="col-2"  style={{textAlign:'center'}}>
                                   Report Type
                                 </div>
-                                <div className="col-2">
-                                  From Date
+                                <div className="col-2"  style={{textAlign:'center'}}>
+                                  Period From
                                 </div>
-                                <div className="col-2">
-                                  To Date
+                                <div className="col-2"  style={{textAlign:'center'}}>
+                                  Period To
                                 </div>       
                                 <div className="col-2">
-                                  
                                 </div>     
-                                <div className="col-2" style={{textAlign:'right'}}>
-                                
+                                <div className="col-1" >
                                 </div>   
 
-                                {reportTypes.map((reportType, index)=> (
-                                  <div className="row" style={{fontSize:'15px'}}>
-                                    <div className="col-2" >
-                                      <p > {index+1}</p>
-                                    </div>
+                                {(customer.report || []).map((report, index)=> (
+                                  <>
                                     <div className="col-2">
-                                      {reportType}
+                                    </div>       
+                                    <div className="col-1">
+                                      {index+1}
                                     </div>
-                                    <div className="col-2">
+                                    <div className="col-2"  style={{textAlign:'center'}}>
+                                      {report.reportType}
+                                    </div>
+                                    <div className="col-2"  style={{textAlign:'center'}}>
                                       11-02-2023
                                     </div>
-                                    <div className="col-2">
+                                    <div className="col-2"  style={{textAlign:'center'}}>
                                       15-02-2023
                                     </div>       
-                                    <div className="col-2">
+                                    <div className="col-2" style={{textAlign:'center'}}>
                                       <Link to='' style={{color:'#ee7170'}}>
                                         See Report
                                       </Link>
-                                      
                                     </div>     
-                                    <div className="col-2" style={{textAlign:'right'}}>
-                                      <Form.Check inline type='checkbox' />
+                                    <div className="col-1" style={{textAlign:'center'}}>
+                                      <Form.Check inline type='checkbox' style={{margin:'0 0 0 0 '}}/>
                                     </div>  
-                                  </div>
+                                  </>
                                 ))}
-
                               </div>
                             </div>
                             </div>
                         </div>
                       </Collapse>
-                    
                   </div>
                 </>
             )) }
