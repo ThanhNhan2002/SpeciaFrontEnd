c231d3449065dfa9eb3e6096f5cb31ece7a55fee
ThanhNhanNguyen <thanhnhan.b12002@gmail.com>
2023-03-27 12:29:06 +1100

    fix logic error: checkboxes array is not reset after submitting the form

--- src/pages/DigitalAssistants/ATOReporter/AddUserModal.js
+++ src/pages/DigitalAssistants/ATOReporter/AddUserModal.js
@@ -28,11 +28,13 @@ export default function AddUserModal ({addUserModalShow, closeAddUserModal, conf
         }
       }
 
-    function confirmAddUser () {
+    function addUser () {
+        confirmAddUser();
         console.log(clientABN);      
         console.log(clientName);
         console.log(adminAccountant);
         console.log(checkedBoxes);
+        setCheckedBoxes([]);
     }
 
     function changeClientABN(e) {
@@ -49,112 +51,9 @@ export default function AddUserModal ({addUserModalShow, closeAddUserModal, conf
 
     return (
         <>
-        {/* <Modal style={{}} show={addUserModalShow} onHide={closeAddUserModal} centered>
-                <Modal.Header style={{backgroundColor: 'rgb(40, 40, 40)', border: 0}} closeButton>
-                    <Modal.Title>Add Customer</Modal.Title>
-                </Modal.Header>
-                <Modal.Body style={{backgroundColor: 'rgb(40, 40, 40)'}}>
-                <div style={{}}>
-                    <div style={{margin:'0 0 18px 0'}}>
-                        <p style={{fontSize: '1.2rem', margin:'0 0 0 0'}}>Client ABN</p>
-                        <Form.Control type="text" onChange={changeClientABN} value = {clientABN} style={{ color: 'white' ,height: '60px', backgroundColor: 'rgba(255,255,255,.2)', borderRadius: '8px', border: 0, fontSize: '1.1rem', paddingLeft: '20px'}} aria-label="ABN" placeholder=''>
-                        </Form.Control>
-                    </div>
-
-                    <div style={{margin:'0 0 18px 0'}}>
-                        <p style={{fontSize: '1.2rem', margin:'0 0 0 0'}}>Client Name</p>
-                        <Form.Control type="text" onChange={changeClientName} style={{ color: 'white' ,height: '60px', backgroundColor: 'rgba(255,255,255,.2)', borderRadius: '8px', border: 0, fontSize: '1.1rem', paddingLeft: '20px'}} aria-label="ABN" placeholder=''>
-                        </Form.Control>
-                    </div>
-
-                    <div style={{margin:'0 0 18px 0'}}>
-                        <p style={{fontSize: '1.2rem', margin:'0 0 0 0'}}>Admin accountant email</p>
-                        <Form.Control type="text" onChange={changeAdminAccountant} style={{ color: 'white' ,height: '60px', backgroundColor: 'rgba(255,255,255,.2)', borderRadius: '8px', border: 0, fontSize: '1.1rem', paddingLeft: '20px'}} aria-label="ABN" placeholder=''>
-                        </Form.Control>
-                    </div>
-
-                    <div class="btn-group" style={{margin:'0 30px 60px 0', display:'flex', flexDirection:'column' }}>
-                        <div><p style={{fontSize: '1.2rem', margin:'0 0 0 0'}}> Select Tax Agent Email</p></div>
-                        <div style={{margin: '3px 0 0 0'}}>
-                            <button class="btn btn-secondary dropdown-toggle" style={{ color:'rgba(255,255,255,.7)', backgroundColor:'rgba(255,255,255,.2)',  height: '60px', border: 'none'}} type="button" id="dropdownMenuClickable" data-bs-toggle="dropdown" data-bs-auto-close="false" aria-expanded="false">
-                                <b>Tax Agent Email</b>
-                            </button>
-                            
-                            <ul class="dropdown-menu" style={{ backgroundColor:'#ffffff33', maxHeight: '100px', overflowY:'scroll'}} aria-labelledby="dropdownMenuClickable">
-                                {taxAgentEmails.map((email, index) => (
-                                        <li style = {{padding: '2px 2px'}}>
-                                            <div class="form-check" style = {{margin: '2px 2px'}} >
-                                                <input class="form-check-input" type="checkbox" id= {`checkBox${email.Id}`} onChange={checkBoxControl.bind(this, email.Id )} />                                         
-                                                <label class="form-check-label" style ={{color:'white'}} for={`checkBox${email.Id}`}>
-                                                    <div>{email.email}</div>
-                                                </label>
-                                            </div>
-                                        </li>
-                                    )
-                                )}
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-                </Modal.Body>
-                <Modal.Footer style={{backgroundColor: 'rgb(40, 40, 40)', border: 0, padding: '20px'}}>
-                    <Button style={{borderRadius: '50px', padding: '12px 18px'}} variant="outline-primary" onClick={closeAddUserModal}>
-                        Cancel
-                    </Button>
-                    <Button style={{borderRadius: '50px', padding: '12px 18px'}} variant="primary" onClick={confirmAddUser}>
-                        Upload
-                    </Button>
-                </Modal.Footer>
-        </Modal> */}
-
         <Modal style={{ backgroundColor: 'rgb(0, 0, 0, 0.4)'}} contentClassName = {digitalAssistantsStyles.modalContent} show={addUserModalShow} onHide={closeAddUserModal} centered>
-        <Modal.Header style={{ border: 0, color: '#ee7170', padding: 0}} closeButton>
-        </Modal.Header>
-        <Modal.Body style={{padding: '10px 30px'}}>
-                {/* <p style={{color: '#ee7170', fontWeight: '500', fontSize: '1.5rem'}} >Add Customer</p>
-                <div style={{margin:'0 0 18px 0'}}>
-                    <p style={{ margin:'0 0 0 0'}}>Client ABN</p>
-                    <Form.Control type="text" onChange={changeClientABN} value = {clientABN} style={{ color: 'black' ,height: '60px', backgroundColor: 'rgba(255,255,255,.2)', borderRadius: '8px', border: '1px solid rgba(0, 0, 0,.2)', fontSize: '1.1rem', paddingLeft: '20px'}} aria-label="ABN" placeholder='ABN'>
-                    </Form.Control>
-                </div>
-
-                <div style={{margin:'0 0 18px 0'}}>
-                    <p style={{margin:'0 0 0 0'}}>Client Name</p>
-                    <Form.Control type="text" onChange={changeClientName} style={{ color: 'black' ,height: '60px', backgroundColor: 'rgba(255,255,255,.2)', borderRadius: '8px', border: '1px solid rgba(0, 0, 0,.2)', fontSize: '1.1rem', paddingLeft: '20px'}} aria-label="ABN" placeholder='Name'>
-                    </Form.Control>
-                </div>
-
-                <div style={{margin:'0 0 18px 0'}}>
-                    <p style={{ margin:'0 0 0 0'}}>Admin accountant email</p>
-                    <Form.Control type="text" onChange={changeAdminAccountant} style={{ color: 'black' ,height: '60px', backgroundColor: 'rgba(255,255,255,.2)', borderRadius: '8px',border: '1px solid rgba(0, 0, 0,.2)', fontSize: '1.1rem', paddingLeft: '20px'}} aria-label="ABN" placeholder='Email'>
-                    </Form.Control>
-                </div>
-
-                <Form.Group className="mb-3" >
-                <div class="btn-group" style={{margin:'0 30px 60px 0', display:'flex', flexDirection:'column' }}>
-                        <div><p style={{fontSize: '1.2rem', margin:'0 0 0 0'}}> Select Tax Agent Email</p></div>
-                        <div style={{margin: '3px 0 0 0'}}>
-                            <button class="btn btn-secondary dropdown-toggle" style={{ color:'rgba(0,0,0,.7)', backgroundColor:'rgba(0,0,0,.2)',  height: '60px', border: 'none'}} type="button" id="dropdownMenuClickable" data-bs-toggle="dropdown" data-bs-auto-close="false" aria-expanded="false">
-                                <b>Tax Agent Email</b>
-                            </button>
-                            
-                            <ul class="dropdown-menu" style={{ backgroundColor:'#white', maxHeight: '100px', overflowY:'scroll'}} aria-labelledby="dropdownMenuClickable">
-                                {taxAgentEmails.map((email, index) => (
-                                        <li style = {{padding: '2px 2px'}}>
-                                            <div class="form-check" style = {{margin: '2px 2px'}} >
-                                                <input class="form-check-input" type="checkbox" id= {`checkBox${email.Id}`} onChange={checkBoxControl.bind(this, email.Id )} />                                         
-                                                <label class="form-check-label" style ={{color:'black'}} for={`checkBox${email.Id}`}>
-                                                    <div>{email.email}</div>
-                                                </label>
-                                            </div>
-                                        </li>
-                                    )
-                                )}
-                            </ul>
-                        </div>
-                </div>
-                </Form.Group> */}
-
+            <Modal.Header style={{ border: 0, color: '#ee7170', padding: 0}} closeButton></Modal.Header>
+            <Modal.Body style={{padding: '10px 30px'}}>
                 <p style={{color: '#ee7170', fontWeight: '500', fontSize: '1.5rem'}} >Add Customer</p>
                 <Form>
                     <Form.Group style={{margin:'0 0 20px 0'}}>
@@ -174,36 +73,35 @@ export default function AddUserModal ({addUserModalShow, closeAddUserModal, conf
 
                     <Form.Group className="mb-3" controlId="formBasicCheckbox">
                         <div class="btn-group" style={{margin:'0 30px 60px 0', display:'flex', flexDirection:'column' }}>
-                                <Form.Label>Select tax agent emails</Form.Label>
-                                <div style={{margin: '3px 0 0 0'}}>
-                                    <button class="btn btn-secondary dropdown-toggle" style={{ color:'white', backgroundColor:'#ee7170',  height: '40px', border: 'none'}} type="button" id="dropdownMenuClickable" data-bs-toggle="dropdown" data-bs-auto-close="false" aria-expanded="false">
-                                        <b>Tax Agent Email</b>
-                                    </button>
-                                    
-                                    <ul class="dropdown-menu" style={{ backgroundColor:'#white', maxHeight: '100px', overflowY:'scroll'}} aria-labelledby="dropdownMenuClickable">
-                                        {taxAgentEmails.map((email, index) => (
-                                                <li style = {{padding: '2px 2px'}}>
-                                                    <div class="form-check" style = {{margin: '2px 2px'}} >
-                                                        <input class="form-check-input" style={{outline:'1px solid #ee7170'}} type="checkbox" id= {`checkBox${email.Id}`} onChange={checkBoxControl.bind(this, email.Id )} />                                         
-                                                        <label class="form-check-label" style ={{color:'rgba(0, 0, 0, 0.5)', fontWeight:'500'}} for={`checkBox${email.Id}`}>
-                                                            <div>{email.email}</div>
-                                                        </label>
-                                                    </div>
-                                                </li>
-                                            )
-                                        )}
-                                    </ul>
-                                </div>
+                            <Form.Label>Select tax agent emails</Form.Label>
+                            <div style={{margin: '3px 0 0 0'}}>
+                                <button class="btn btn-secondary dropdown-toggle" style={{ color:'white', backgroundColor:'#ee7170',  height: '40px', border: 'none'}} type="button" id="dropdownMenuClickable" data-bs-toggle="dropdown" data-bs-auto-close="false" aria-expanded="false">
+                                    <b>Tax Agent Email</b>
+                                </button>
+                                
+                                <ul class="dropdown-menu" style={{ backgroundColor:'#white', maxHeight: '100px', overflowY:'scroll'}} aria-labelledby="dropdownMenuClickable">
+                                    {taxAgentEmails.map((email, index) => (
+                                        <li style = {{padding: '2px 2px'}}>
+                                            <div class="form-check" style = {{margin: '2px 2px'}} >
+                                                <input class="form-check-input" style={{outline:'1px solid #ee7170'}} type="checkbox" id= {`checkBox${email.Id}`} onChange={checkBoxControl.bind(this, email.Id )} />                                         
+                                                <label class="form-check-label" style ={{color:'rgba(0, 0, 0, 0.5)', fontWeight:'500'}} for={`checkBox${email.Id}`}>
+                                                    <div>{email.email}</div>
+                                                </label>
+                                            </div>
+                                        </li>
+                                        )
+                                    )}
+                                </ul>
+                            </div>
                         </div>
                     </Form.Group>
-
                 </Form>
-        </Modal.Body>
-        <Modal.Footer style={{border: 0, padding: '30px'}}>
-        <p style={{ color: '#ee7170', fontWeight: '600', cursor: 'pointer'}}  onClick={confirmAddUser}>
-            Upload
-        </p>
-        </Modal.Footer>
+            </Modal.Body>
+            <Modal.Footer style={{border: 0, padding: '30px'}}>
+                <p style={{ color: '#ee7170', fontWeight: '600', cursor: 'pointer'}}  onClick={addUser}>
+                    Upload
+                </p>
+            </Modal.Footer>
         </Modal>
         </>
     )
